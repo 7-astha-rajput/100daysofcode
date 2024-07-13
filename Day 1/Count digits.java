b892c7a5131a3dfb7cbd287ace5051ded7f68069
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-07-13 19:24:18 +0530

    Create Count digits.java

---
+++ Day 1/Count digits.java
@@ -0,0 +1,16 @@
+class Solution{
+    static int evenlyDivides(int N){
+        // code here
+        int originalNumber = N;
+        int count = 0;
+
+        while (N > 0) {
+            int ld = N % 10;
+            N = N / 10;
+            if (ld != 0 && originalNumber % ld == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
