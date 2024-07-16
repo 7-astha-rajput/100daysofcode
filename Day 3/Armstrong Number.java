95d7e4c83ef516c1ed3753cb774273489740c381
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-07-16 23:04:21 +0530

    Update Armstrong Number.java

--- Day 3/Armstrong Number.java
+++ Day 3/Armstrong Number.java
@@ -1,1 +1,34 @@
-.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    // Method to calculate the sum of the cubes of the digits
+    public static boolean isArmstrong(int x) {
+        int sum = 0;
+        int original = x;
+        while (x > 0) {
+            int ld = x % 10;
+            sum = sum + (ld * ld * ld);
+            x = x / 10;
+        }
+        return sum == original;
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+        // Read number of test cases
+        int T = scanner.nextInt();
+        // Iterate through all test cases
+        for (int i = 0; i < T; i++) {
+            // Read each number
+            int x = scanner.nextInt();
+            // Check if the number is an Armstrong number
+            if (isArmstrong(x)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
