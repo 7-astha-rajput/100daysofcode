1d2ade65521fb4264275236d05fd93e670594cf1
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-08-04 23:42:35 +0530

    Update fibbonacci.java

--- Day 10/fibbonacci.java
+++ Day 10/fibbonacci.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0; // fib(0)
+        int b = 1; // fib(1)
+        int fib = 0;
+
+        // Calculate Fibonacci numbers iteratively
+        for (int i = 2; i <= n; i++) {
+            fib = a + b; // fib(i) = fib(i-1) + fib(i-2)
+            a = b; // Move forward in the sequence
+            b = fib; // Move forward in the sequence
+        }
+
+        return fib;
+    }
+}
 
