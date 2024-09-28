c047a4013954930dcae47714c47ad19c95f5516a
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-09-28 15:44:19 +0530

    Update and rename q2.java to slargestelementinanarray.java

---
+++ Day 14./slargestelementinanarray.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int largest = arr[0];
+        int slargest = -1;
+        int n = arr.length;
+        for(int i = 0; i< n ; i++){
+            if(arr[i] > largest){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else
+                if(arr[i]<largest && arr[i] > slargest)
+                slargest = arr[i];
+                
+            
+        }
+        return slargest;
+    }
+}
