e06d8e98c980c609f23b89d9c10ceb02a843ea77
Astha Rajput <98215717+7-astha-rajput@users.noreply.github.com>
2024-12-14 23:59:34 +0530

    Update Missing Number.java

--- Day 18/Missing Number.java
+++ Day 18/Missing Number.java
@@ -4,7 +4,7 @@ class Solution {
         int xor1 = 0;
         int xor2 = 0;
         for(int i = 0; i <n; i++){ 
-            xor2 = xor2 ^ nums[i];
+            xor2 = xor2 ^ nums[i]; 
             xor1 = xor1 ^ (i+1);
         } 
      
